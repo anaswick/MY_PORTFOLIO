0a70bc2ba1ec16a27e1fd11df9a2219651012bdf
Anas Wicaksono <anas.wicaksono25@gmail.com>
2023-06-06 16:46:08 +0700

    Update mytens.sql

--- SQL/DIGITAL_OPERATIONS_AND_ASSURANCE/mytens.sql
+++ SQL/DIGITAL_OPERATIONS_AND_ASSURANCE/mytens.sql
@@ -1,1 +1,6 @@
-
+#NUMBER OF COMPLAINT TICKETS PER MONTH
+select DATE_FORMAT(Periode, '%Y-%m') MONTH, count(*)
+from dm_sapu_jagad dsj2
+where product = 'Mytens gobeyond' and (Periode between '2023-01-01 00:00:00' AND '2023-05-31 23:59:59')
+group by 1
+order by 1

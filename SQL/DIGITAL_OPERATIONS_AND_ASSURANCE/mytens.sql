3adef7ddd513c368a0d97009324656647690f79b
Anas Wicaksono <anas.wicaksono25@gmail.com>
2023-06-17 13:06:21 +0700

    Update mytens.sql

--- SQL/DIGITAL_OPERATIONS_AND_ASSURANCE/mytens.sql
+++ SQL/DIGITAL_OPERATIONS_AND_ASSURANCE/mytens.sql
@@ -17,13 +17,13 @@ from dm_all_ticket dat
 where Product = 'Mytens gobeyond' and (TicketDate between '2023-01-01 00:00:00' AND '2023-05-31 23:59:59')
 
 #AVERAGE TIME TO RESOLVED PER MONTH BY SYMPTOMS
-select DATE_FORMAT(Periode, '%Y-%m') ,Symptom , avg(ResolvedTime_Second/3600)
+select DATE_FORMAT(Periode, '%Y-%m') ,Symptom , avg(ResolvedTime_Second/3600), min(ResolvedTime_Second/3600), max(ResolvedTime_Second/3600)
 from dm_sapu_jagad dsj
 where product = 'MyTens GoBeyond' and (Periode between '2023-01-01 00:00:00' AND '2023-05-31 23:59:59')
 group by 1,2
 
 #AVERAGE TIME TO RESOLVED PER MONTH FOR ALL SYMPTOMS
-select DATE_FORMAT(Periode, '%Y-%m') MONTH, avg(ResolvedTime_Second/3600)
+select DATE_FORMAT(Periode, '%Y-%m') MONTH, avg(ResolvedTime_Second/3600), min(ResolvedTime_Second/3600), max(ResolvedTime_Second/3600)
 from dm_sapu_jagad dsj
 where product = 'MyTens GoBeyond' and (Periode between '2023-01-01 00:00:00' AND '2023-05-31 23:59:59')
 group by 1

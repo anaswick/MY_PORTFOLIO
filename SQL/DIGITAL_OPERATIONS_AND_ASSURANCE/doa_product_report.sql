d7ec27938b8cefee2f82daacc9faa59094dab0e3
Anas Wicaksono <anas.wicaksono25@gmail.com>
2024-02-26 02:53:13 +0000

    Update doa_product_report.sql

--- SQL/DIGITAL_OPERATIONS_AND_ASSURANCE/doa_product_report.sql
+++ SQL/DIGITAL_OPERATIONS_AND_ASSURANCE/doa_product_report.sql
@@ -32,3 +32,29 @@ where ticket_created between '2024-01-01 00:00:00' and '2024-01-31 23:59:59' and
 group by 1,2
 order by 1,3 desc,2;
 
+-- tren 3 bulan terakhir
+select left(ticket_created,7) BULAN, count(ticket_number) 'JUMLAH TIKET'
+from dm_oca
+where ticket_created between '2023-10-01 00:00:00' and '2024-01-31 23:59:59' and product = 'Indibizpay' -- ganti range tanggal dan nama produk
+group by 1
+
+
+-- hitung tiket yang comply
+
+SELECT 
+    product,
+    SUM(CASE 
+            WHEN priority = 'low' AND MTTRep_Minute < 60 THEN 1
+            WHEN priority = 'medium' AND MTTRep_Minute < 360 THEN 1
+            WHEN priority = 'high' AND MTTRep_Minute < 720 THEN 1
+            ELSE 0
+        END) AS compliant_tickets,
+		COUNT(*) AS total_tickets,
+(compliant_tickets / total_tickets) *100 'SLA COMPLIANCE RATE'
+FROM 
+dm_oca
+where ticket_created between '2023-10-01 00:00:00' and '2024-01-31 23:59:59' 
+GROUP BY 
+product
+order by total_tickets DESC;
+;
